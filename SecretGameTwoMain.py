2248530ebace0145bc6791890671919c8cba4ff8
Ángel Álvarez <kreandostudio@gmail.com>
2018-12-11 18:43:00 +0100

    cambios

--- SecretGameTwoMain.py
+++ SecretGameTwoMain.py
@@ -9,13 +9,15 @@ print "Bienvenidos a Secret Game!"
 
 try:
     guess = int(raw_input("Introduzca el número a adivinar, ¿lo adivinarás? Tiene que ser del 1 al 20. ¡ADELANTE!: "))
-    if guess <= 20:
-        if guess == secret:
-            print "¡ACERTASTE! INCREIBLE!!"
+    if guess >= 0:
+        if guess <= 20:
+            if guess == secret:
+                print "¡ACERTASTE! INCREIBLE!!"
+            else:
+                print "OOOOHHHH! Po va ser que no... FALLASTE!! Era", secret
         else:
-            print "OOOOHHHH! Po va ser que no... FALLASTE!! Era", secret
+            print "¿Crees que son pocos numeros? No te preocupes, trabajaremos para poner mas, de momento es hasta 20. Lo siento."
     else:
-        print "¿Crees que son pocos numeros? No te preocupes, trabajaremos para poner mas, de momento es hasta 20. Lo siento."
-
+        print "NO estan permitidos los numeros negativos! PERDISTE!!"
 except ValueError:
     print "MAL CHAVAL/CHAVALA!! Debe ser un numero (del 1 al 20)!"
